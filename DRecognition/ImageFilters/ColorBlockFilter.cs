32f91c8c7bdd61da9da24dfe5e2b9c914c76d7b9
Rulee Chen <rulee@live.cn>
2016-10-02 14:31:07 +0800

    update tesseract resources

---
+++ DRecognition/ImageFilters/ColorBlockFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRecognition.ImageFilters
+{
+    public class ColorBlockFilter : IImageFilter
+    {
+        public ColorBlockFilter(int findArgbColor, int replaceArgbColor, int maxNearPoints)
+        {
+            FindColor = findArgbColor;
+            ReplaceColor = replaceArgbColor;
+            MaxNearPoints = maxNearPoints;
+        }
+
+        public int FindColor { get; set; }
+        public int ReplaceColor { get; set; }
+        public int MaxNearPoints { get; set; }
+
+        public Bitmap Apply(Bitmap bitmap)
+        {
+            var findColor = Color.FromArgb(FindColor);
+            var replaceColor = Color.FromArgb(ReplaceColor);
+            
+
+        }
+
+        public string GetCode()
+        {
+            return $"new {GetType().Name}({FindColor}, {ReplaceColor}, {MaxNearPoints})";
+        }
+    }
+}
